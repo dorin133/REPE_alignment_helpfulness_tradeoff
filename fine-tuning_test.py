bd7d1eee17cca65c0dcff9f19c712a3c7ec32683
Binyamin-R <binyamin007@gmail.com>
2024-08-20 17:07:14 +0300

    adding fine-tuning experiment

--- fine-tuning_test.py
+++ fine-tuning_test.py
@@ -2,7 +2,7 @@ import torch
 import numpy as np
 from transformers import (DataCollatorWithPadding, AutoModelForCausalLM, AutoTokenizer, TrainingArguments, Trainer)
 from torch.utils.data import DataLoader
-from peft import LoraConfig, get_peft_model
+from peft import LoraConfig, get_peft_model, LoftQConfig
 from datasets import load_dataset
 from trl import DPOTrainer, DPOConfig
 import pdb
@@ -44,13 +44,14 @@ test_dataset = test_dataset.filter(filter_function)
 train_dataset = train_dataset.map(preprocess_function)
 test_dataset = test_dataset.map(preprocess_function)
 
-
+loftq_config = LoftQConfig(loftq_bits=4)
 peft_config = LoraConfig(
     task_type="CAUSAL_LM",
-    r=2,
+    r=1,
     lora_alpha=32,
     lora_dropout=0.1,
-    target_modules=["q_proj", "k_proj", "v_proj"]
+    target_modules=["q_proj", "k_proj", "v_proj"],
+    loftq_config=loftq_config,
 )
 
 model = get_peft_model(model, peft_config)
@@ -67,8 +68,8 @@ training_args = DPOConfig(
     output_dir="./lora_finetuned_model",
     overwrite_output_dir=True,
     num_train_epochs=num_epochs,
-    per_device_train_batch_size=per_device_batch_size,
-    per_device_eval_batch_size=per_device_batch_size,
+    per_device_train_batch_size=1,
+    per_device_eval_batch_size=1,
     weight_decay=0.01,
     logging_dir="./logs",
     save_steps=save_steps,  # Save the model every 100 steps
@@ -86,7 +87,7 @@ dpo_trainer = DPOTrainer(
     tokenizer=tokenizer,
     max_prompt_length=512,
     max_length=2048,
-    peft_config=peft_config,
+    # peft_config=peft_config,
 )
 
 dpo_trainer.train()
