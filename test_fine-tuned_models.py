09e4aced8595b1c9e381f9948f0f66d510458387
Binyamin-R <binyamin007@gmail.com>
2024-08-27 12:37:32 +0300

    generating with finetuned models

--- test_fine-tuned_models.py
+++ test_fine-tuned_models.py
@@ -9,7 +9,8 @@ import numpy as np
 from datasets import load_dataset
 import random
 import pdb
-
+import os
+import gc
 from generate_code_with_REPE import sample_model
 
 
@@ -18,6 +19,12 @@ def load_model(model_path):
     tokenizer = AutoTokenizer.from_pretrained(model_path, padding_side="left", legacy=False, token=True)
     model.to(device)
     return model, tokenizer
+
+def clear_memory():
+    gc.collect()
+    torch.cuda.empty_cache()
+    if torch.cuda.is_available():
+        torch.cuda.synchronize()
 
 torch.manual_seed(42)
 random.seed(42)
@@ -33,15 +40,18 @@ model_dir = '/cs/labs/shashua/binyamin/REPE_alignment_helpfulness_tradeoff/lora_
 model_subdirs = [d for d in os.listdir(model_dir) if os.path.isdir(os.path.join(model_dir, d))]
 
 generation_path = 'fine-tuned_model_generations.json'
-generation_dict = dict()
+generation_dict = read_json_if_exists(generation_path)
 for model_subdir in model_subdirs:
     model_path = os.path.join(model_dir, model_subdir)
-    generation_dict[model_subdir] = dict()
+    if model_subdir not in generation_dict:
+        generation_dict[model_subdir] = dict()
     print(f"Testing model in: {model_path}")
 
     model, tokenizer = load_model(model_path)
 
     for q_id, q in human_eval_dict.items():
+        if q_id in generation_dict[model_subdir]:
+            continue
         print(f"Question: {q_id}")
         print(f"Model: {model_subdir}")
         print("Generating samples...")
@@ -51,4 +61,11 @@ for model_subdir in model_subdirs:
 
     with open(generation_path, 'w') as file:
         json.dump(generation_dict, file)
+
+    # Clear memory
+    del model
+    del tokenizer
+    clear_memory()
+    print("Memory cleared")
+    print()
 
