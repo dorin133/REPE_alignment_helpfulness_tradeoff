e1ed678c2b7d2dacf351fa873f39591b2531fd1a
Binyamin-R <binyamin007@gmail.com>
2024-09-30 14:16:33 +0300

    adding human eval generation and testing

---
+++ test_code_generation.py
@@ -0,0 +1,173 @@
+from datasets import load_dataset
+import json
+import re
+import numpy as np
+import matplotlib.pyplot as plt
+from code_runner import test_humaneval_function
+
+def parser_code(model_response):
+    split_model_output = model_response.split("```")
+    if len(split_model_output) == 1:
+        return False
+    fixed_code = split_model_output[1].replace('python', '').replace('Python', '')
+    return fixed_code
+
+
+def get_import_lines(code: str) -> str:
+    # Split the code into lines
+    lines = code.split('\n')
+    # Filter lines that contain import statements
+    import_lines = [line for line in lines if line.strip().startswith('import') or line.strip().startswith('from')]
+    # Join the import lines back into a single string
+    return '\n'.join(import_lines)
+
+
+def find_first_non_indent_line(text, start_index):
+    lines = text.splitlines()
+    for i in range(start_index, len(lines)):
+        # Check if the line does not start with a space or a tab
+        if lines[i] and not lines[i].startswith((' ', '\t')):
+            return i
+    return -1  # Return -1 if no such line is found
+
+
+def find_first_def_line(text, start_index):
+    lines = text.splitlines()
+    for i in range(start_index, len(lines)):
+        if 'def ' in lines[i]:
+            return i
+    return -1
+
+
+def extract_line_range(text, start_line, end_line):
+    lines = text.splitlines()
+    return '\n'.join(lines[start_line:end_line])
+
+
+def extract_function(mixed_string, function_name, starting_index=0):
+    if starting_index is None:
+        return None, None
+    def_idx_fk = find_first_def_line(mixed_string, starting_index)
+    if def_idx_fk == -1:
+        return None, None
+    end_idx_fk = find_first_non_indent_line(mixed_string, def_idx_fk+1)
+    if end_idx_fk == -1:
+        return None, None
+    return extract_line_range(mixed_string, def_idx_fk, end_idx_fk), end_idx_fk
+
+
+def extract_function_2(mixed_string, function_name):
+    pattern = rf"```\s*\n*def\s+{re.escape(function_name)}\s*\([^)]*\):(?:(?!\n```)[\s\S])*\n```"
+    match = re.search(pattern, mixed_string, re.MULTILINE)
+    if match:
+        function_code = match.group(0)
+        function_code = re.sub(r'^```\s*\n*', '', function_code)
+        function_code = re.sub(r'\n*```\s*$', '', function_code)
+        return function_code.strip()
+    else:
+        return None
+
+
+def test_human_eval_dataset(all_generations, data_dict):
+    results = dict()
+    for coeff in all_generations:
+        curr_generations = all_generations[coeff]
+        success_perc_list = []
+        for key in curr_generations:
+            print(key)
+            curr_generation_batch = curr_generations[key]
+            curr_problem = data_dict[key]
+
+            full_success_list = []
+            for i in range(len(curr_generation_batch)):
+                entry_point = curr_problem['entry_point']
+                curr_answer = curr_generation_batch[i]
+                ADD_PROMPT = f'def {entry_point}' not in curr_answer
+                if ADD_PROMPT:
+                    curr_answer = f"{curr_problem['prompt']}\n{curr_answer}"
+                curr_imports = get_import_lines(curr_answer)
+                function_codes_to_try = []
+                option1, index = extract_function(curr_answer + "\nbuffer_text", curr_problem['entry_point'])
+                function_codes_to_try.append(option1)
+                for curr_function_code in function_codes_to_try:
+                    if curr_function_code is None:
+                        is_pass = False
+                        continue
+                    try:
+                        is_pass = test_humaneval_function(curr_problem, curr_function_code)
+                    except Exception as e:
+                        is_pass = False
+                    if is_pass:
+                        break
+                full_success_list.append(is_pass)
+
+            success_perc = np.sum(full_success_list) / len(full_success_list)
+            success_perc_list.append(success_perc)
+
+        results[float(coeff)] = np.average(success_perc_list), np.std(success_perc_list) / len(success_perc_list)**0.5
+
+    return results
+
+
+def plot_results(keys, averages, stds):
+    averages = np.array(averages)
+    stds = np.array(stds)
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(keys, averages)
+    plt.fill_between(keys, averages - stds, averages + stds, alpha=0.2)
+    plt.xlabel('coefficients')
+    plt.ylabel('mean success rate')
+    plt.title('Human eval success rate vs. REPE coefficients')
+    plt.grid(True, linestyle='--', alpha=0.7)
+
+    plt.tight_layout()
+    plt.show()
+
+
+def plot_fine_tuned_models_results(results_dict):
+    results = [results_dict["/cs/labs/shashua/binyamin/models/Meta-Llama-3.1-8B"]]
+    results_dict.pop("/cs/labs/shashua/binyamin/models/Meta-Llama-3.1-8B")
+    checkpoints = list(results_dict.keys())
+    checkpoints = sorted(checkpoints, key=lambda x: int(x.split('-')[-1]))
+    print(checkpoints)
+    for i in checkpoints:
+        curr_results = results_dict[i]
+        results.append(curr_results)
+
+    avgs = [res[0] for res in results]
+    stds = [res[1] for res in results]
+    plot_results(np.arange(len(results)), avgs, stds)
+
+
+def main():
+    human_eval_data = load_dataset("openai/openai_humaneval")
+    human_eval_dict = {q['task_id']: q for q in human_eval_data['test']}
+    all_gen_dict = {}
+    gens_paths = ['code_generations/code_generations_results_23_09_all_human_eval_bias_is_race.json',
+                  'code_generations/code_generations_results_23_09_all_human_eval_bias_is_race_second.json',
+                  'code_generations/code_generations_results_23_09_all_human_eval_bias_is_race_coeff=45.json',
+                  'code_generations/code_generations_results_23_09_all_human_eval_bias_is_race_second_ceof=-4-5.json']
+    for path in gens_paths:
+        curr_gen = open(path)
+        curr_gen = json.load(curr_gen)
+        for key in curr_gen:
+            if key not in all_gen_dict:
+                all_gen_dict[key] = curr_gen[key]
+            else:
+                for q_key in curr_gen[key]:
+                    if q_key not in all_gen_dict[key]:
+                        all_gen_dict[key][q_key] = curr_gen[key][q_key]
+                    else:
+                        all_gen_dict[key][q_key] += curr_gen[key][q_key]
+
+    results = test_human_eval_dataset(all_gen_dict, human_eval_dict)
+    results = dict(sorted(results.items()))
+    sorted_keys = list(results.keys())
+    avgs = [key[0] for key in results.values()]
+    stds = [key[1] for key in results.values()]
+    plot_results(sorted_keys, avgs, stds)
+
+
+if __name__ == '__main__':
+    main()
---
+++ utils.py
@@ -0,0 +1,118 @@
+import json
+import os
+import torch
+from transformers import AutoTokenizer, pipeline, AutoModelForCausalLM
+import gc
+import numpy as np
+import random
+from peft import LoraConfig, get_peft_model, PeftModel
+
+question_template = \
+"""[INST] <<SYS>>
+You are a helpful, respectful and honest assistant. You will be given a python function to complete.
+Complete the function correctly. Separate the code of the function from the rest of your message.
+Avoid unnecessary indentation in your answer. Only give one answer. 
+<</SYS>>
+{user_prompt} [/INST]"""
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+def read_json_if_exists(file_path):
+    if os.path.exists(file_path):
+        try:
+            with open(file_path, 'r') as file:
+                return json.load(file)
+        except json.JSONDecodeError:
+            print(f"Error: {file_path} is not a valid JSON file.")
+            return dict()
+    else:
+        print(f"Error: {file_path} does not exist.")
+        return dict()
+
+
+def sample_model(model, tokenizer, question, num_samples=32, batch_size=2, question_template_for_sample=None,
+                 device=device):
+    if question_template_for_sample is None:
+        question_template_for_sample = question_template
+    prompt = question_template_for_sample.format(user_prompt=question['prompt'])
+    q_encoding = tokenizer.encode_plus(prompt, return_tensors="pt", padding=True)
+    input_ids = q_encoding['input_ids'].to(device)
+    attn_mask = q_encoding['attention_mask'].to(device)
+    num_batches = num_samples // batch_size
+
+    all_answers = []
+    for j in range(num_batches):
+        with torch.inference_mode():
+            outputs = model.generate(input_ids, max_new_tokens=1800, temperature=1.0, do_sample=True,
+                                     top_p=0.95, attention_mask=attn_mask,
+                                     return_dict_in_generate=True,
+                                     pad_token_id=tokenizer.pad_token_id, num_return_sequences=batch_size)
+        # decode the input only
+        partial_given_answers = [tokenizer.decode(output_sequence[:input_ids.shape[1]], skip_special_tokens=True) for
+                                 output_sequence in outputs.sequences]
+        # decode the entire output, and remove the input from it
+        curr_answers = [
+            tokenizer.decode(outputs.sequences[i], skip_special_tokens=True).replace(partial_given_answers[i],
+                                                                                     "").replace(
+                '<s>', "").replace('</s>', "") for i in range(len(partial_given_answers))]
+        all_answers += curr_answers
+
+    return all_answers
+
+def load_model(model_path):
+    base_model = os.path.join('/cs/labs/shashua/binyamin/models/', "Meta-Llama-3.1-8B")
+    model = AutoModelForCausalLM.from_pretrained(base_model, torch_dtype=torch.float16, token=True,
+                                                 local_files_only=True, cache_dir=None, use_cache=False).eval()
+    tokenizer = AutoTokenizer.from_pretrained(base_model, padding_side="left", legacy=False, token=True,
+                                              local_files_only=True, cache_dir=None, use_cache=False)
+
+    if not model_path == base_model:
+        # Load the fine-tuned LoRA weights
+        model = PeftModel.from_pretrained(model, model_path)
+        model = model.merge_and_unload()
+    model.to(device)
+    return model, tokenizer
+
+def clear_memory(*objects):
+    # Delete any objects passed as arguments
+    for obj in objects:
+        del obj
+
+    # Collect garbage
+    gc.collect()
+
+    # Clear CUDA cache if available
+    if torch.cuda.is_available():
+        torch.cuda.empty_cache()
+        torch.cuda.synchronize()
+
+def set_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
+def get_checkpoint_models(model_dir, base_model = None):
+    if base_model is None:
+        base_model = os.path.join('/cs/labs/shashua/binyamin/models/', "Meta-Llama-3.1-8B")
+
+    model_subdirs = [d for d in os.listdir(model_dir) if os.path.isdir(os.path.join(model_dir, d))]
+    model_subdirs = sorted(model_subdirs, key=lambda x: int(x.split('-')[-1]))
+    model_dirs = [os.path.join(model_dir, model_subdir) for model_subdir in model_subdirs]
+    model_dirs.insert(0, base_model)
+    return model_dirs
+
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+def print_color(text, color):
+    print(color + text + bcolors.ENDC)
